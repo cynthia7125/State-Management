428b7a06b46eb2da13e882006bd2ba853a78fe7d
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-01 09:48:21 +0300

    Client-side navigation via navlink and link

--- src/Products.jsx
+++ src/Products.jsx
@@ -3,23 +3,26 @@ import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
 import { useParams } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
+import { Link } from "react-router-dom";
 
 export default function Products() {
   const [size, setSize] = useState();
-  const {category} = useParams();
+  const { category } = useParams();
 
-  const { data: products, loading, error } = useFetch(
-    "products?category=" + category
-  );
+  const {
+    data: products,
+    loading,
+    error,
+  } = useFetch("products?category=" + category);
 
-   function renderProduct(p) {
+  function renderProduct(p) {
     return (
       <div key={p.id} className="product">
-        <a href="/">
+        <Link to={`/${category}/${p.id}`}>
           <img src={`/images/${p.image}`} alt={p.name} />
           <h3>{p.name}</h3>
           <p>${p.price}</p>
-        </a>
+        </Link>
       </div>
     );
   }
@@ -29,24 +32,24 @@ export default function Products() {
 
   if (error) throw error;
   if (loading) return <Spinner />;
-  if (products.length === 0) return <PageNotFound />
+  if (products.length === 0) return <PageNotFound />;
 
   return (
     <>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select
-              id="size"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+      <section id="filters">
+        <label htmlFor="size">Filter by Size:</label>{" "}
+        <select
+          id="size"
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+        >
+          <option value="">All sizes</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
+      </section>
+      <section id="products">{filteredProducts.map(renderProduct)}</section>
     </>
   );
 }
